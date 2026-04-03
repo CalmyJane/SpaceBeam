0877981893a8bd4a7bf87437c78503567e8de86b
Calmy Jane <info@calmyjane.com>
2026-04-03 21:53:19 +0200

    bumped version to 3.4 (code 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 6
-        versionName = "3.3"
+        versionName = "3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
