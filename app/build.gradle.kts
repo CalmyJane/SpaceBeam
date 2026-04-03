43b26662c2049b222f04b698be617333f8ad915a
Calmy Jane <info@calmyjane.com>
2026-04-03 21:52:05 +0200

    bumped version to 3.3 (code 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.calmyjane.spacebeam"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "3.1"
+        versionCode = 6
+        versionName = "3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
