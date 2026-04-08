b491c2e93e5ebdc1c14c17d170d18faf2b359fbd
Calmy Jane <info@calmyjane.com>
2026-04-08 21:49:13 +0200

    Bumped to version 4.0 with feedback loops and more.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.calmyjane.spacebeam"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
-        versionName = "3.5"
+        versionCode = 8
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
